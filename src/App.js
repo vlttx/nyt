40aa45de1a632ba3ab3992c75b0d4b3ef6239f2e
VBTX <jv.svedaite@gmail.com>
2019-11-13 15:33:17 -0600

    update search functionality

--- src/App.js
+++ src/App.js
@@ -1,65 +1,89 @@
-import React from 'react';
-import './App.css';
-import fetch from 'isomorphic-fetch'
-import { CardList } from './components/card-list/CardList'
-import { SearchBox } from './components/search-box/SearchBox'
+import React from "react";
+import "./App.css";
+import fetch from "isomorphic-fetch";
+import { CardList } from "./components/card-list/CardList";
+import { SearchBox } from "./components/search-box/SearchBox";
 
-
-const API_KEY = process.env.REACT_APP_NYT_API_KEY
-const BASE_URL = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=${API_KEY}&query=`
-
-
+const API_KEY = process.env.REACT_APP_NYT_API_KEY;
+const BASE_URL = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=${API_KEY}&query=`;
 
 class App extends React.Component {
-
   state = {
     searchTerm: "",
     reviews: [],
     criticsPicks: [],
     searchPicks: ""
-  }
+  };
 
-  componentDidMount(){
-    fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=${API_KEY}&critics-pic`)
+  componentDidMount() {
+    fetch(
+      `https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=${API_KEY}&critics-pic`
+    )
       .then(res => res.json())
       .then(resJson => this.setState({ criticsPicks: resJson.results }));
-  };
+  }
 
-  handleSubmit = (event) => {
+  handleSubmit = event => {
     event.preventDefault();
 
-     fetch(BASE_URL.concat(this.state.searchTerm))
+    fetch(BASE_URL.concat(this.state.searchTerm))
       .then(res => res.json())
-      .then(res => this.setState({ reviews: res.results }));
-
+      .then(res => {
+        if (res.has_more) {
+          this.setState({ reviews: res.results });
+        } else {
+          alert("No reviews matched your search!");
+        }
+      });
   };
 
-  handleChange = (event) => {
-    this.setState({searchTerm: event.target.value});
+  handleChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
 
-  }
-
-  handlePicksChange = (e) => {
-    this.setState({searchPicks: e.target.value})
-  }
-
+  handlePicksChange = e => {
+    this.setState({ searchPicks: e.target.value });
+  };
 
   render() {
     const { criticsPicks, searchPicks } = this.state;
-    const filteredPicks = criticsPicks.filter(pick => pick.headline.toLowerCase().includes(searchPicks.toLowerCase()))
-  return (
-    <div className="App">
-      <form onSubmit={this.handleSubmit}>
-      <h1>Movie Reviews from the New York Times</h1>
-      <h2>Search Movie Reviews</h2>
-      <input className="search" type="text" onChange={this.handleChange} placeholder={"Search all reviews"} />
-      </form>
-      <br/>
-      <br/>
-     { this.state.reviews.length === 0 ? <div><h2>Critics' Picks:</h2><SearchBox placeholder={"Search picks"} handleChange={this.handlePicksChange}/><br/><br/>
-     { this.state.searchPicks !== "" ? <CardList reviews={filteredPicks}/>:<CardList reviews={this.state.criticsPicks}/>}</div> : <CardList reviews={this.state.reviews}/>}
-    </div>
-  );
-}
+    const filteredPicks = criticsPicks.filter(pick =>
+      pick.headline.toLowerCase().includes(searchPicks.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <h1>Movie Reviews from the New York Times</h1>
+          <h2>Search Movie Reviews</h2>
+          <input
+            className="search"
+            type="text"
+            onChange={this.handleChange}
+            placeholder={"Search all reviews"}
+          />
+        </form>
+        <br />
+        <br />
+        {this.state.reviews.length === 0 ? (
+          <div>
+            <h2>Critics' Picks:</h2>
+            <SearchBox
+              placeholder={"Search by headline"}
+              handleChange={this.handlePicksChange}
+            />
+            <br />
+            <br />
+            {this.state.searchPicks !== "" ? (
+              <CardList reviews={filteredPicks} />
+            ) : (
+              <CardList reviews={this.state.criticsPicks} />
+            )}
+          </div>
+        ) : (
+          <CardList reviews={this.state.reviews} />
+        )}
+      </div>
+    );
+  }
 }
 export default App;
